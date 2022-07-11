498891c969855e6239e478699df8c45d249e1ea9
mecclesgoogle <markeccles@google.com>
2022-07-11 17:02:18 +1000

    first commit

---
+++ firestore_latency_test.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+import timer
+
+from google.cloud import firestore
+import google.oauth2.credentials
+
+credentials = google.oauth2.credentials.Credentials(os.getenv('TOKEN'))
+db = firestore.Client(credentials=credentials, project=os.getenv('PROJECT_ID'))
+
+transaction = db.transaction()
+city_ref = db.collection(u'cities').document(u'9nVWHXAvZtb0du50uVj5')
+
+@firestore.transactional
+def transactional_get(transaction, city_ref):
+    city_ref.get(transaction=transaction)
+
+def plain_get(city_ref):
+    city_ref.get()
+
+def perf_test():
+  t = timer.Timer()
+  limit = 100
+  n = 0
+  while n < limit:
+    n += 1
+    with t:
+      transactional_get(transaction, city_ref)
+  print('type,avg,p99')
+  print(f'transactional,{t.elapsed / limit},{np.percentile(t.stages, 99)}')
+
+  t2 = timer.Timer()
+  n = 0
+  while n < limit:
+    n += 1
+    with t2:
+      plain_get(city_ref)
+  print(f'nontransactional,{t2.elapsed / limit},{np.percentile(t2.stages, 99)}')
+
+perf_test()
+
